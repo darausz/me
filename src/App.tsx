18a295fb0e4afb27533cb08a1c11c3e81bd20ea3
dareaus <darauszhang@gmail.com>
2024-10-08 19:27:37 -0400

    added some assets

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Switch from './components/Switch'
 import Contact from './components/Contact'
+import arrow from "./assets/Arrow.png"
 
 
 function App() {
@@ -12,15 +13,27 @@ function App() {
     <>
       {view === "top" ?
         <>
-          <button className=' bg-slate-400 absolute left-0 top-1/2 h-16 w-16' onClick={() => { setPanel((panel) => panel - 1) }}>Left</button>
+          {/* <button className=' bg-slate-400 absolute left-0 top-1/2 h-16 w-16' onClick={() => { setPanel((panel) => panel - 1) }}>Left</button> */}
+          <div className=' absolute left-0 top-1/2 h-16 w-16 rotate-180' onClick={() => { setPanel((panel) => panel - 1) }} style={{ cursor: 'pointer' }}>
+            <img src={arrow}></img>
+          </div>
           <Switch panel={panel} />
-          <button className=' bg-slate-400 absolute right-0 top-1/2 h-16 w-16' onClick={() => { setPanel((panel) => panel + 1) }}>Right</button>
-          <button className=' bg-slate-400 absolute left-1/2 bottom-0 h-16 w-16' onClick={() => { setView("bottom") }}>Bottom</button>
+          <div className=' absolute right-0 top-1/2 h-16 w-16' onClick={() => { setPanel((panel) => panel + 1) }} style={{ cursor: 'pointer' }}>
+            <img src={arrow}></img>
+          </div>
+          {/* <button className=' bg-slate-400 absolute left-1/2 bottom-0 h-16 w-16' onClick={() => { setView("bottom") }}>Bottom</button> */}
+          <div className='absolute left-1/2 bottom-0 h-16 w-16 rotate-90' onClick={() => { setView("bottom") }} style={{ cursor: 'pointer' }}>
+            <img src={arrow}></img>
+          </div>
+          
         </>
         :
         <>
           <Contact />
-          <button className=' bg-slate-400 absolute left-1/2 top-0 h-16 w-16' onClick={() => { setView("top") }}>top</button>
+          {/* <button className=' bg-slate-400 absolute left-1/2 top-0 h-16 w-16' onClick={() => { setView("top") }}>top</button> */}
+          <div className='absolute left-1/2 top-0 h-16 w-16 -rotate-90' onClick={() => { setView("top") }} style={{ cursor: 'pointer' }}>
+            <img src={arrow}></img>
+          </div>
         </>
       }
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,25 +1,39 @@
+import shelf1 from "../assets/Shelf1.png";
+import light from "../assets/Light.png"
+
 export default function Home() {
-  return(
+  return (
     <div className='wall'>
-        <div className='bg-white w-32 h-12 absolute left-1/4 top-0'>light</div>
-        <div className='display absolute left-1/2 top-1/4'>
-          <div className='item bold text-4xl'>Daraus Zhang</div>
-          <div className='shelf bg-amber-800 w-64 h-3'></div>
-        </div>
-        <div className='display absolute left-1/3 top-1/2'>
-            <div className='item bold text-4xl'>Software Engineer</div>
-          <div className='shelf bg-amber-800 w-64 h-3'></div>
-        </div>
-        <div className='display absolute left-3/4 top-3/4'>
-        <div className='flex justify-between items-end'>
-            <div className='item bold text-4xl'>Books</div>
-            <div className='plant bg-green-700 w-16 h-16'>plant</div>
-          </div>
-          <div className='shelf bg-amber-800 w-64 h-3'></div>
-        </div>
-        <div className='welcome absolute left-0 bottom-1/4'>
-          Welcome! Feel free to look around (wrap text bubble)
+      <div className='h-32 absolute left-1/4 top-0'>
+        <img src={light}></img>
+      </div>
+      <div className='display absolute left-1/2 top-1/4'>
+        <div className='item bold text-4xl'>Daraus Zhang</div>
+        {/* <div className='shelf bg-amber-800 w-64 h-3'></div> */}
+        <div className='shelf'>
+          <img src={shelf1}></img>
         </div>
       </div>
+      <div className='display absolute left-1/3 top-1/2'>
+        <div className='item bold text-4xl'>Software Engineer</div>
+        {/* <div className='shelf bg-amber-800 w-64 h-3'></div> */}
+        <div className='shelf'>
+          <img src={shelf1}></img>
+        </div>
+      </div>
+      <div className='display absolute left-3/4 top-3/4'>
+        <div className='flex justify-between items-end'>
+          <div className='item bold text-4xl'>Books</div>
+          <div className='plant bg-green-700 w-16 h-16'>plant</div>
+        </div>
+        {/* <div className='shelf bg-amber-800 w-64 h-3'></div> */}
+        <div className='shelf'>
+          <img src={shelf1}></img>
+        </div>
+      </div>
+      <div className='welcome absolute left-0 bottom-1/4'>
+        Welcome! Feel free to look around (wrap text bubble)
+      </div>
+    </div>
   )
 }
