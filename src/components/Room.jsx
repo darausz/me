b263b94b6f12b883b1195905e3ae752484a702eb
dareaus <darauszhang@gmail.com>
2024-11-14 00:03:45 -0500

    added window

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -7,7 +7,8 @@ import shelf from "../assets/shelf.png";
 import bookshelf from "../assets/Bookshelf.png"
 import background from "../assets/Background.png"
 import setup from "../assets/Setup.png"
-import { useState } from "react";
+import openWindow from "../assets/window.png"
+import {useState} from "react"
 
 export default function Room() {
   // const [darkMode, setDarkMode] = useState(false)
@@ -19,16 +20,19 @@ export default function Room() {
         <img src={background}></img>
       </div>
       <Light />
-      <div className='name flex flex-col mx-auto absolute left-1/2 top-1/4'>
+      <div className='name'>
         <div className='blockLetters'>&nbsp;DARAUS ZHANG</div>
         {/* <div className='shelf bg-amber-800 w-64 h-3'></div> */}
-        <div className='wallShelf'>
+        <div className='shelf'>
           <img src={shelf}></img>
         </div>
       </div>
       {/* <div className="absolute left-1/2 bottom-1/2">ADD DIPLOMA HERE</div> */}
       <div className="bookcase">
         <img className="highlight overflow-hidden" src={bookshelf} style={{ cursor: 'pointer' }} onClick={() => {nav("/bookshelf")}}></img>
+      </div>
+      <div className="window">
+        <img src={openWindow}></img>
       </div>
       <div className="setup">
         <img className="highlight" src={setup} onClick={() => {nav("/projects")}}></img>
